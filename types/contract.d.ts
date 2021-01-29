dea1603de6c38276305e49f0a801337533ff8edd
Nazar Duchak <nazar@iovlabs.org>
2021-01-29 15:58:09 +0200

    remove build stuff

--- types/contract.d.ts
+++
@@ -1,8 +0,0 @@
-import { AbiItem } from 'web3-utils';
-import Web3Contract from 'web3-eth-contract';
-export declare class Contract extends Web3Contract {
-    readonly abi: AbiItem[];
-    readonly address: string;
-    readonly name: string;
-    constructor(abi: AbiItem[], address: string, name: string);
-}
